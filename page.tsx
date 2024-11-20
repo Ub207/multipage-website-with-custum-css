5d9692b35174a36b114acf07f6732a565156a727
Ub207 <111739646+Ub207@users.noreply.github.com>
2024-11-20 22:02:12 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import HomeContent from "@/components/HomeContent";
+
+export default function Home(){
+  return(
+    <div>
+      <HomeContent/>
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
